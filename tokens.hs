290a9038d4e2e095021835e1ffa560ea34304a9e
william <4yellowpeppers@gmail.com>
2015-03-26 20:53:29 -0400

    refactor complete - still need to improve tokenize function

--- tokens.hs
+++ tokens.hs
@@ -13,25 +13,39 @@
 -- The Porter stemmer was written by Dmitry Antonyuk, and can
 -- be found at: "tartarus.org/~martin/PorterStemmer/haskell.txt"
 
-import Data.Char
-import Data.List.Split
+import Data.Char (chr, toLower)
+import Data.List.Split (endByOneOf)
 
--- A fairly comprehensive list of non-letter characters
+-- Filenames
+inputFilename = "assignment3-input.txt"
+stopwordFilename = "assignment3-stopwords.txt"
+tokensOutFile = "tokenized"
+stopwordsOutFile = "no_stopwords"
+
+-- a fairly comprehensive list of non-letter characters
 nonLetters = map chr $ [00 .. 64] ++ [91 .. 96] ++ [123 .. 126]
 
 main = do
 	-- Read in both the input and stopword files
-	contents <- readFile "assignment3-input.txt"
-	sw <- readFile "assignment3-stopwords.txt"
-	
-	-- Write the tokenizer and stopwords-free outputs to separate files
-	writeFile "output1-tokens.txt" $ tokenizer contents
-	writeFile "output2-stopwords.txt" $ stopFilter (lines $ tokenizer contents) (lines sw)
+	inputFile <- readFile inputFilename
+	stopwordFile <- readFile stopwordFilename
+	let swList = lines stopwordFile
+	let tokens = tokenize inputFile
+	writeFile tokensOutFile (unlines $ tokens)
+	writeFile stopwordsOutFile (unlines $ rmStopwords tokens swList)
 
--- The tokenizer function splits the input text at any non-letter character
-tokenizer :: [Char] -> String
-tokenizer n = unlines $ filter (not.null) $ endByOneOf nonLetters $ map toLower n
+-- converts string to lowercase
+lowerString :: String -> String
+lowerString = map toLower
 
--- The stopFilter function strips all stopwords from the tokenizer output
-stopFilter :: [String] -> [String] -> String
-stopFilter n m = unlines $ filter (\x -> not (elem x m)) n
+-- removes null values from list of lists
+rmNulls :: [[a]] -> [[a]]
+rmNulls = filter (not.null)
+
+-- splits input string at any non-letter character
+tokenize :: String -> [String]
+tokenize s = rmNulls $ endByOneOf nonLetters (lowerString s)
+
+-- strips all stopwords from tokens list
+rmStopwords :: [String] -> [String] -> [String]
+rmStopwords tks sws = filter (`notElem` sws) tks
